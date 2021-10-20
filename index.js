a1ce4b1f87a49ea3405069231450cb7e43a5c192
sergiocamacho09 <trabajointernetalameda@gmail.com>
2021-10-20 09:17:29 +0200

    primer commit

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-var messages = [
-  {
-    author: "Sergio",
-    text: "CALVOOOOOOOOOOOOOOO",
-  },
-];
+let aux = [];
 /*aplicación express que pasa a un servidor http*/
 const express = require('express');
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+/*Usamos la librería file system para leer nuestro archivo JSON con las tareas a realizar*/
+const fs = require('fs');
+
 app.use(express.static('www'));
 
 io.on("connection", function(socket){
   console.log("nuevo cliente | Socket: " + socket.id);
 
-  socket.emit('messages', messages);
-
-  /*socket.on("message_evt", function(message){
-    console.log(socket.id, JSON.parse(message));
-    socket.emit("message_evt", message);
-  });*/
-  socket.on("new-message", function(data){
-    messages.push(data);
-
-    socket.emit("messages", messages);
-  })
+  socket.on("new-task", function(task){
+    /*Usamos el array auxiliar para guardar nuestras nuevas tareas*/
+    aux.push(task);
+    /*Convertimos nuestro array auxilira a formato JSON*/
+    var new_task = JSON.stringify(aux, null, '\t');
+    /*Leemos nuestro fichero y añadimos nuestro array con todas las tareas*/
+    fs.writeFile('task.json', new_task, function(err){
+      if(err) throw err;
+      console.log("Tarea añadida!");
+    });
+  });
 });
 
 server.listen(3000, function() {
-  console.log('Servidor encendido')
+  console.log('Servidor conectado')
 });
---
+++ www/todo.js
@@ -0,0 +1,71 @@
+var socket = io();
+
+/*Lista con todas las actividades a realizar por el usuario*/
+let tasks = [];
+
+/*Variables para el control táctil*/
+let start_x = 0;
+let end_x = 0;
+let start_time = 0;
+const TIME_THRESHOLD = 200;
+const SPACE_THRESHOLD = 200;
+
+/*Controlamos el control táctil de nuestra aplicación*/
+document.addEventListener("touchstart", function(e){
+    e.preventDefault();
+    start_x = e.targetTouches[0].screenX;
+    start_time = e.timeStamp;
+}, {passive: false});
+
+document.addEventListener("touchmove", function(e){
+    e.preventDefault();
+    end_x = e.changedTouches[0].screenX;
+}, {passive: false});
+
+document.addEventListener("touchend", function(e){
+    e.preventDefault();
+    end_time = e.timeStamp;
+    if(end_time - start_time < TIME_THRESHOLD && end_x - start_x > SPACE_THRESHOLD){
+        remove();
+    }
+});
+
+/*Añade una tarea a la lista de tareas de nuestra aplicación*/
+function add() {
+    /*Variable que controla los id's dentro de nuestra lista*/
+    let counter = 1;
+    /*Variable que controla nuestros id's dentro de nuestro fichero task.json*/
+    let id = tasks.length + 1;
+    /*Creamos nuestra nueva tarea*/
+    if (document.getElementById("task").value != '') {
+        var task = {
+            "id": id,
+            "title": document.getElementById("task").value,
+            "done": false,
+        };
+        /*Añadimos nuestra tarea al inicio de nuestra lista*/
+        tasks.unshift(task);
+        socket.emit("new-task", task);
+        /*Reasignamos los id's a las tareas de nuestra lista*/
+        for (i = 0; i < tasks.length; i++) {
+            tasks[i].id = counter;
+            counter++;
+        };
+    } else {
+        console.log("Por favor, añade una tarea a la lista");
+    }
+    
+    
+
+    //console.log(tasks);
+    return false;
+}
+
+function insertInHTML(array){
+
+}
+
+/*Elimina una tarea de nuestra lista*/
+function remove(e) {
+   
+}   
