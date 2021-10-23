695b6a24c2f743f1cb76dc680d4130e7dfc7138c
sergiocamacho09 <trabajointernetalameda@gmail.com>
2021-10-23 13:06:24 +0200

    add y insercción en html terminado

--- index.js
+++ index.js
@@ -12,9 +12,25 @@ app.use(express.static('www'));
 io.on("connection", function(socket){
   console.log("nuevo cliente | Socket: " + socket.id);
 
+  /*Recargamos nuestro array auxiliar con las tareas guardadas en nuestro fichero JSON para evitar que se pierdan*/
+  fs.readFile('task.json', function(err,data){
+    if(err) throw err;
+    aux = JSON.parse(data);
+    console.log(aux);
+  });
+
   socket.on("new-task", function(task){
+    /*Variable que controla los ID's dentro de nuestra lista de tareas*/
+    let counter = aux.length;
+    console.log(counter);
+    /*Creamos nuestra nueva tarea con todos los datos para añadirla al array auxiliar*/
+    var add_task = {
+        "id": counter,
+        "title": task,
+        "done": false
+    }
     /*Usamos el array auxiliar para guardar nuestras nuevas tareas*/
-    aux.push(task);
+    aux.push(add_task);
     /*Convertimos nuestro array auxilira a formato JSON*/
     var new_task = JSON.stringify(aux, null, '\t');
     /*Leemos nuestro fichero y añadimos nuestro array con todas las tareas*/
@@ -23,14 +39,13 @@ io.on("connection", function(socket){
       console.log("Tarea añadida!");
     });
   });
+
+
 });
 
 app.get("/tasks/all_tasks", function(req,res){
-  console.log("ENTRA");
   fs.readFile('task.json', function(err,data){
     if(err) throw err;
-    console.log("entra");
-    console.log(JSON.parse(data));
     res.send(data);
   });
 });
--- www/todo.js
+++ www/todo.js
@@ -10,57 +10,62 @@ let start_time = 0;
 const TIME_THRESHOLD = 200;
 const SPACE_THRESHOLD = 200;
 
+/*Obtenemos la lista de tareas al conectarnos con el cliente por primera vez*/
+load_tasks();
+/*Añadimos nuestras tareas a la aplicación HTML*/
+insertInHTML();
+
 /*Controlamos el control táctil de nuestra aplicación*/
-/*document.addEventListener("touchstart", function(e){
-    e.preventDefault();
-    start_x = e.targetTouches[0].screenX;
-    start_time = e.timeStamp;
-}, {passive: false});
+// document.addEventListener("touchstart", function(e){
+//     e.preventDefault();
+//     start_x = e.targetTouches[0].screenX;
+//     start_time = e.timeStamp;
+// }, {passive: false});
 
-document.addEventListener("touchmove", function(e){
-    e.preventDefault();
-    end_x = e.changedTouches[0].screenX;
-}, {passive: false});
+// document.addEventListener("touchmove", function(e){
+//     e.preventDefault();
+//     end_x = e.changedTouches[0].screenX;
+// }, {passive: false});
 
-document.addEventListener("touchend", function(e){
-    e.preventDefault();
-    end_time = e.timeStamp;
-    if(end_time - start_time < TIME_THRESHOLD && end_x - start_x > SPACE_THRESHOLD){
-        remove();
-    }
-});*/
+// document.addEventListener("touchend", function(e){
+//     e.preventDefault();
+//     end_time = e.timeStamp;
+//     if(end_time - start_time < TIME_THRESHOLD && end_x - start_x > SPACE_THRESHOLD){
+//         remove();
+//     }
+// });
+
+
 
 /*Añade una tarea a la lista de tareas de nuestra aplicación*/
 function add() {
-    /*Variable que controla los id's dentro de nuestra lista*/
-    let counter = 1;
-    /*Variable que controla nuestros id's dentro de nuestro fichero task.json*/
-    let id = tasks.length + 1;
     /*Creamos nuestra nueva tarea*/
     if (document.getElementById("task").value != '') {
-        var task = {
-            "id": id,
-            "title": document.getElementById("task").value,
-            "done": false,
-        };
-        /*Añadimos nuestra tarea al inicio de nuestra lista*/
-        //console.log(load_tasks());
-        //tasks.unshift(task);
+        var task = document.getElementById("task").value;
+        /*Envíamos nuestra tarea al servidor para que la registre en el JSON*/
         socket.emit("new-task", task);
-        /*Reasignamos los id's a las tareas de nuestra lista*/
-        for (i = 0; i < tasks.length; i++) {
-            tasks[i].id = counter;
-            counter++;
-        };
+        /*Recuperamos la lista de tareas para añadirla a nuestro array de tareas*/
+        load_tasks();
+        console.log(tasks);
     } else {
         console.log("Por favor, añade una tarea a la lista");
     }
-    //console.log(tasks);
-}
+};
 
-// function insertInHTML(array) {
 
-// }
+/*Añadimos las tareas a nuestra aplicación HTML*/
+function insertInHTML() {
+    /*Seleccionamos el div general en el que irán nuestras tareas*/
+    var previous_div2 = document.querySelector('#current_tasks');
+    /*Creamos los divs que contendrán cada una de nuestras tareas */
+    var text = "";
+    for(i = tasks.length - 1; i >= 0 ; i--){
+        text += "<div class = 'task'>" + tasks[i].title + "</div>"; 
+    };
+    previous_div2.innerHTML = text;
+};
+
+
 
 // /*Elimina una tarea de nuestra lista*/
 // function remove(e) {
@@ -68,11 +73,14 @@ function add() {
 // }
 
 
+
 async function load_tasks() {
     const response = await fetch("/tasks/all_tasks");
     const data = await response.text();
     tasks = JSON.parse(data);
-}
+    insertInHTML();
+    console.log(tasks);
+};
 
 
-load_tasks();
+
